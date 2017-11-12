5887b06058980573f539bb6792454c3aaff8e0f2
Artur Signell <artur@vaadin.com>
2017-11-12 13:35:03 +0200

    Delete duplicate files and fix script

--- tools/updateElementVersions.js
+++ tools/updateElementVersions.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-oldversion=require('./package.json').version;
-newversion=$npm_package_version
+oldversion=require('../package.json').version;
+newversion=process.env.npm_package_version
 
 const replace = require('replace-in-file');
 const options = {
   files: '*.html',
-  from: $oldversion,
-  to: $newversion,
+  from: oldversion,
+  to: newversion,
 };
 
 try {
   const updatedFiles = replace.sync(options);
   const simpleGit = require('simple-git')(".");
   simpleGit.add(updatedFiles);
-  console.log('Modified files:', changes.join(', '));
+  console.log('Modified files:', updatedFiles.join(', '));
 }
 catch (error) {
   console.error('Error occurred:', error);
