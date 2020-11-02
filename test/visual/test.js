93dc8981969c199c55e3ee3a72cd680c3f298172
web-padawan <iamkulykov@gmail.com>
2020-11-02 15:17:32 +0200

    Convert visual tests to hermione-esm [skip ci]

--- test/visual/test.js
+++ test/visual/test.js
@@ -1,20 +1,12 @@
-gemini.suite('vaadin-upload', function (rootSuite) {
-  function wait(actions, find) {
-    actions.wait(5000);
-  }
-
-  function goToAboutBlank(actions, find) {
-    // Firefox stops responding on socket after a test, workaround:
-    return actions.executeJS(function (window) {
-      window.location.href = 'about:blank'; // just go away, please!
-    });
-  }
-
-  rootSuite.before(wait).after(goToAboutBlank);
+describe('vaadin-upload', () => {
+  const locator = '#upload-tests[data-ready]';
 
   ['lumo', 'material'].forEach((theme) => {
-    gemini.suite(`default-tests-${theme}`, function (suite) {
-      suite.setUrl(`/default.html?theme=${theme}`).setCaptureElements('#default-tests').capture(`vaadin-upload`);
+    it(`${theme}-default`, function () {
+      return this.browser
+        .url(`upload.html?theme=${theme}`)
+        .waitForVisible(locator, 10000)
+        .assertView(`${theme}-default`, locator);
     });
   });
 });
