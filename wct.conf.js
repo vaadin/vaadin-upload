6b404d90b04f85ffd23a58fd494c3d99998c5bfb
web-padawan <iamkulykov@gmail.com>
2020-11-02 11:27:49 +0200

    magi-cli: prepare for modulizer

--- wct.conf.js
+++ wct.conf.js
@@ -14,21 +14,6 @@ module.exports = {
         ]
       }
     },
-    // MAGI REMOVE START
-    istanbul: {
-      dir: './coverage',
-      reporters: ['text-summary', 'lcov'],
-      include: [
-        '**/vaadin-upload/src/*.html'
-      ],
-      exclude: [],
-      thresholds: {
-        global: {
-          statements: 92
-        }
-      }
-    }
-    // MAGI REMOVE END
   },
 
   registerHooks: function(context) {
