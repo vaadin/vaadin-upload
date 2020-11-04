51933423ffd12971c274e5ca7c9c1763a2465ab8
web-padawan <iamkulykov@gmail.com>
2020-11-04 10:11:36 +0200

    Fix typings copy-paste

--- test/typings/upload.types.ts
+++ test/typings/upload.types.ts
@@ -1,64 +1,64 @@
 import { UploadFile } from '../../vaadin-upload';
 import '../../src/vaadin-upload';
 
-const select = document.createElement('vaadin-upload');
-
 const assert = <T>(value: T) => value;
 
-select.addEventListener('max-files-reached-changed', (event) => {
+const upload = document.createElement('vaadin-upload');
+
+upload.addEventListener('max-files-reached-changed', (event) => {
   assert<boolean>(event.detail.value);
 });
 
-select.addEventListener('file-reject', (event) => {
+upload.addEventListener('file-reject', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<string>(event.detail.error);
 });
 
-select.addEventListener('files-changed', (event) => {
+upload.addEventListener('files-changed', (event) => {
   assert<UploadFile[]>(event.detail.value);
 });
 
-select.addEventListener('upload-before', (event) => {
+upload.addEventListener('upload-before', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-start', (event) => {
+upload.addEventListener('upload-start', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-progress', (event) => {
+upload.addEventListener('upload-progress', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-response', (event) => {
+upload.addEventListener('upload-response', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-success', (event) => {
+upload.addEventListener('upload-success', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-error', (event) => {
+upload.addEventListener('upload-error', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-retry', (event) => {
+upload.addEventListener('upload-retry', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-abort', (event) => {
+upload.addEventListener('upload-abort', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<XMLHttpRequest>(event.detail.xhr);
 });
 
-select.addEventListener('upload-request', (event) => {
+upload.addEventListener('upload-request', (event) => {
   assert<UploadFile>(event.detail.file);
   assert<FormData>(event.detail.formData);
 });
